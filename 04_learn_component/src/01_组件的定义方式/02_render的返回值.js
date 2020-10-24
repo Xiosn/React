b93ec53418dcf71c833e9352fda39fa7babadaee
Xiosn <Xiosn@github.com>
2020-10-25 00:09:45 +0800

    render函数可返回的类型

---
+++ 04_learn_component/src/01_组件的定义方式/02_render的返回值.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+/*
+  不要瞎返回
+  可返回的类型
+  1 React元素
+  2 数组或fragments
+  3 Portals   可以渲染子节点到不同的DOM子树中
+  4 字符串或数值类型    会被渲染成文本节点
+  5 布尔类型或null 什么都不渲染
+*/
+
+export default class App extends Component {
+  render() {
+    return aaa
+  }
+}
