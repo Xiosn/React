8398cf4a9575c669ccae83ef17f55efeb91c5311
xiaohs <xiaohs@yamyamyam.net>
2020-10-26 15:56:41 +0800

    父传子通信-属性验证（prop-types）

---
+++ 04_learn_component/src/03_组件之间的通信/04_父传子通信-属性验证.js
@@ -0,0 +1,29 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+
+function ChildCpn(props) {
+  return (
+  <div>
+    <h2>{props.name}</h2>
+    <h2>{props.age}</h2>
+    <h2>{props.height}</h2>
+  </div>
+  )
+}
+
+ChildCpn.propTypes={
+  name:PropTypes.string,
+  age:PropTypes.number,
+  height:PropTypes.number
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <ChildCpn name='xiosn' age="18" height='1.88'/>
+        <ChildCpn name='xiaohs' age='20' height='1.85'/>
+      </div>
+    )
+  }
+}
