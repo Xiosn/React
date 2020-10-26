001852117d7f4c6c815ea74b5ffd86f2e82bde4e
xiaohs <xiaohs@yamyamyam.net>
2020-10-26 14:59:00 +0800

    _父传子通信-函数组件传值

--- 04_learn_component/src/index.js
+++ 04_learn_component/src/index.js
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom';
 // import App from './01_组件的定义方式/01_组件的定义';
 // import App from './02_组件的生命周期/App';
 // import App from './03_组件之间的通信/01_组件间的嵌套';
-import App from './03_组件之间的通信/02_父传子通信-类组件';
+// import App from './03_组件之间的通信/02_父传子通信-类组件';
+import App from './03_组件之间的通信/03_父传子通信-函数组件';
 
 ReactDOM.render( <App />, document.getElementById('root'));
