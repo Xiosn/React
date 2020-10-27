990014d460760dc2502f64b880d8e8700590a3a5
xiaohs <xiaohs@yamyamyam.net>
2020-10-27 17:27:21 +0800

    组件通信的案例

--- 04_learn_component/src/index.js
+++ 04_learn_component/src/index.js
@@ -7,6 +7,7 @@ import ReactDOM from 'react-dom';
 // import App from './03_组件之间的通信/02_父传子通信-类组件';
 // import App from './03_组件之间的通信/03_父传子通信-函数组件';
 // import App from './03_组件之间的通信/04_父传子通信-属性验证';
-import App from './03_组件之间的通信/05_子传父通信-函数传递';
+// import App from './03_组件之间的通信/05_子传父通信-函数传递';
+import App from './04_组件通信的案例/App';
 
 ReactDOM.render( <App />, document.getElementById('root'));
