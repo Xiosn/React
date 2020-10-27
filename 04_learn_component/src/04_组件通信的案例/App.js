84bb143d97ffdf002858eaf9dadca04c720ecb2f
xiaohs <xiaohs@yamyamyam.net>
2020-10-27 18:03:43 +0800

    组件通信的案例-TabControl切换效果

--- 04_learn_component/src/04_组件通信的案例/App.js
+++ 04_learn_component/src/04_组件通信的案例/App.js
@@ -1,10 +1,11 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import TabControl from './TabControl';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        
+        <TabControl titles={['新款','流行','精选']}/>        
       </div>
     )
   }
--- 04_learn_component/src/04_组件通信的案例/TabControl.js
+++ 04_learn_component/src/04_组件通信的案例/TabControl.js
@@ -1,1 +1,18 @@
-encodeURIComponent
+import { Component } from "react";
+import PropTypes from 'prop-types';
+export default class TabControl extends Component {
+  render() {
+    const {titles} = this.props;
+    return (
+      <div>
+        {titles.map((item, index)=>{
+          return <h2 key={index}>{item}</h2>
+        })}
+      </div>
+    )
+  }
+}
+
+TabControl.propTypes= {
+  titles: PropTypes.array
+}
