8b18d1e9861d059d400052722c5e13a09999f737
xiaohs <xiaohs@yamyamyam.net>
2020-10-28 17:29:34 +0800

    跨组件通信-props层层传递

--- 04_learn_component/src/06_跨组件的通信/01_跨组件通信-props.js
+++ 04_learn_component/src/06_跨组件的通信/01_跨组件通信-props.js
@@ -1,10 +1,46 @@
 import React, { Component } from 'react'
+function ProfileHeader(props) {
+  const nickName = props.nickName;
+  const level = props.level;
+  return (
+    <div>
+      <h2>我的昵称：{nickName}</h2>
+      <h2>我的等级：{level}</h2>
+    </div>
+  )
+}
 
-export default class 01_跨组件通信-props extends Component {
+function ProFile(props) {
+  const nickName = props.nickName;
+  const level = props.level;
+  return (
+    <div>
+      {/* <ProfileHeader nickName={nickName} level={level}/> */}
+      <ProfileHeader {...props}/>
+      <ul>
+        <li>档案1</li>
+        <li>档案2</li>
+        <li>档案3</li>
+        <li>档案4</li>
+      </ul>
+    </div>
+  )
+}
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state={
+      nickName:'kobe',
+      level:99
+    }
+  }
   render() {
+    // const {nickName, level} = this.state;
     return (
       <div>
-        
+        {/* <ProFile nickName={nickName} level={level}/> */}
+        <ProFile {...this.state}/>
       </div>
     )
   }
