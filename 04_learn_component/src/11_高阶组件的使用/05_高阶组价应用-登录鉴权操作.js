944c8a48091d4845d5b8a0505ac6a91b795bc2ca
xiaohs <xiaohs@yamyamyam.net>
2020-11-19 10:19:37 +0800

    高阶组件在登录鉴权的运用

---
+++ 04_learn_component/src/11_高阶组件的使用/05_高阶组价应用-登录鉴权操作.js
@@ -0,0 +1,42 @@
+import React,{PureComponent} from 'react';
+
+//登录页面
+class LoginPage extends PureComponent {
+  render() {
+    return (
+      <div>
+        LoginPge
+      </div>
+    )
+  }
+}
+//购物车
+class CartPage extends PureComponent {
+  render() {
+    return (
+      <div>
+        CartPage
+      </div>
+    )
+  }
+}
+//包装一层统一处理
+function withAuth(WrappedComponent) {
+  return props=>{
+    const {isLogin} = props;
+    if (isLogin) {
+      return <WrappedComponent {...props}/>
+    }else return <LoginPage/>
+  }
+}
+
+const AuthCartPage  = withAuth(CartPage);
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <AuthCartPage isLogin={false}/>
+      </div>
+    )
+  }
+}
