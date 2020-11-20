51774ad788755143804b22ecd042fbcd17f41d4f
xiaohs <xiaohs@yamyamyam.net>
2020-11-20 15:17:59 +0800

    脱离于父组件的元素内容（portals)

---
+++ 04_learn_component/src/12_组件内容·补充/02_portals的使用.js
@@ -0,0 +1,44 @@
+import React, { PureComponent } from 'react';
+import ReactDOM from 'react-dom';
+
+/**
+ * ReactDOM.createPortal()
+ * 某些情况下 我们希望渲染的内容独立于父组件 甚至是独立于当前挂载到的DOM元素中（默认都是挂载到id为root的DOM元素上）
+ * 参数一：（child）是任何可渲染的React子元素 例如一个元素 字符串或fragment
+ * 参数二：（container）是一个DOM元素
+ * 
+ * 本案例是在html中新定义了个 DIV（Div为屏幕居中） 并将子组件渲染到上面 独立于 root
+ */
+
+ //居中 "盒子" 可将要居中的 子组件传入
+class Modal extends PureComponent {
+  render() {
+    return ReactDOM.createPortal(
+      this.props.children,
+      document.getElementById("modal")
+    )
+  }
+}
+
+class Home extends PureComponent {
+  render() {
+    return (
+      <div>
+        <h2>Home</h2>
+        <Modal>
+          <h2>Title</h2>
+        </Modal>
+      </div>
+    )
+  }
+}
+
+export default class App extends PureComponent {
+  render() {
+    return (
+      <div>
+        <Home/>
+      </div>
+    )
+  }
+}
