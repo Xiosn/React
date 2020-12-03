2df492e7f0a9d2f8290dcef1b00402ee41f473a8
xiaohs <xiaohs@yamyamyam.net>
2020-12-03 18:06:04 +0800

    styled-components的阶级使用

--- 05_react_style/src/04_styled-components/app/index.js
+++ 05_react_style/src/04_styled-components/app/index.js
@@ -1,8 +1,7 @@
 import React, { PureComponent } from 'react';
-
+import styled from 'styled-components'
 import Home from '../home';
 import Profile from '../profile';
-
 
 /**
  * css in js 的样式库
@@ -11,6 +10,18 @@ import Profile from '../profile';
  */
 
 
+ /**
+  * 当前相同按钮样式时 可通过继承的方式 详见 按钮一 按钮二
+  */
+ const HYButton = styled.button`
+  padding:10px 20px;
+  border-color:red;
+  color:red;
+ `
+ const HYButtonTwo = styled(HYButton)`
+  color:blue;
+  background-color:yellow;
+ `
 export default class App extends PureComponent {
   render() {
     return (
@@ -18,6 +29,8 @@ export default class App extends PureComponent {
         <Home />
         <hr />
         <Profile />
+        <HYButton>按钮一</HYButton>
+        <HYButtonTwo>按钮二</HYButtonTwo>
       </div>
     )
   }
--- 05_react_style/src/04_styled-components/profile/index.js
+++ 05_react_style/src/04_styled-components/profile/index.js
@@ -1,4 +1,21 @@
 import React, { PureComponent } from 'react';
+import styled from 'styled-components'
+
+/**
+ * attrs({}) 本身是一个函数 传入一个对象 返回值也是一个函数
+ * 在下面定义样式的时候 可以使用模板字符串 传入一个箭头函数 会自动将attrs中的值(一般是写死的) 通过props直接调用
+ * 
+ * 如何动态的设置属性值 例颜色？
+ * 将color={this.state.color}设置为组件属性 会自动添加到props 供styled中的属性设置调用
+ */
+const HYInput = styled.input.attrs({
+  placeholder:"ABCDE",
+  bColor:"red"
+})`
+  background-color:lightblue;
+  border-color:${props=>props.bColor};
+  color:${props=>props.color};
+`
 
 /**
  * 特点:
@@ -19,8 +36,8 @@ export default class Profile extends PureComponent {
   render() {
     return (
       <div>
-        <input type="password"/>
-       
+        <HYInput type="password" color={this.state.color}/>
+        <input type='password'/>
         <h2>我是Profile的标题</h2>
         <ul>
           <li>设置列表1</li>
