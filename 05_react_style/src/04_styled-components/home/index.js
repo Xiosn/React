d8fd6ee59743b95106dfa31e208cb66777d9f8b8
xiaohs <xiaohs@yamyamyam.net>
2020-12-03 16:11:06 +0800

    styledComponents的基本使用

--- 05_react_style/src/04_styled-components/home/index.js
+++ 05_react_style/src/04_styled-components/home/index.js
@@ -1,14 +1,49 @@
 import React, { PureComponent } from 'react';
+import styled from 'styled-components';
 
+const HomeWrapper = styled.div`
+  font-size:50px;
+  color:red;
+  .banner{
+    background-color:blue;
+    span{
+      color:#fff;
+      //当前span同时是active的时候
+      &.active{
+        color:red;
+      }
+      
+      //支持鼠标移入移出
+      &:hover{
+        color:green;
+      }
+
+      //支持伪类伪元素
+      &::after{
+        content:'aaa'
+      }
+    }
+    /* .active {
+      color:red;
+    } */
+  }
+`
+
+const TitleWrapper = styled.h2`
+  text-decoration:underline;
+`
 export default class Home extends PureComponent {
   render() {
     return (
-      <div>
-        <span>轮播图1</span>
-        <span>轮播图2</span>
-        <span>轮播图3</span>
-        <span>轮播图4</span>
-      </div>
+      <HomeWrapper>
+        <TitleWrapper>我是home的标题</TitleWrapper>
+        <div className='banner'>
+          <span>轮播图1</span>
+          <span className="active">轮播图2</span>
+          <span>轮播图3</span>
+          <span>轮播图4</span>
+        </div>
+      </HomeWrapper>
     )
   }
 }
